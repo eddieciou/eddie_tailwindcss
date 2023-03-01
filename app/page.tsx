fc6eb93c2c124fa8a13f38e090f4ea697adfffe2
eddieciou <a589658gur@gmail.com>
2023-03-01 18:06:47 +0800

    添加eslint-plugin-tailwindcss

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,19 @@
 export default function Home() {
   return (
-    <div>
-      <div className='bg-black p-6'>
-        <div className='bg-green-200'>G</div>
+    <div className='h-screen bg-yellow-400 bg-[url("/img/store.jpg")] bg-cover'>
+      <div className=''>
+        <div className=''>
+          <h1>
+            金魚系列-圖文滿版練習
+            <small>跟著 AMOS 來切版</small>
+          </h1>
+          <h2>使用到的技巧</h2>
+          <p>
+            使用到 CSS reset，inline & block ，flex-direction 的原理
+            <br />
+            漸層色彩與多重背景撰寫方式，100vh 螢幕高度!
+          </p>
+        </div>
       </div>
     </div>
   );
