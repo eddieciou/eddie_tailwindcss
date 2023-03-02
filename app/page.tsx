3bbbe29295d2fce72eddd58439de00a1004e0e0f
eddieciou <a589658gur@gmail.com>
2023-03-02 10:03:14 +0800

    chapter 1

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,9 @@
 export default function Home() {
   return (
-    <div className='h-screen bg-yellow-400 bg-[url("/img/store.jpg")] bg-cover'>
-      <div className=''>
-        <div className=''>
-          <h1>
-            金魚系列-圖文滿版練習
-            <small>跟著 AMOS 來切版</small>
-          </h1>
-          <h2>使用到的技巧</h2>
-          <p>
-            使用到 CSS reset，inline & block ，flex-direction 的原理
-            <br />
-            漸層色彩與多重背景撰寫方式，100vh 螢幕高度!
-          </p>
+    <div className='radial-blue grid min-h-screen place-content-center'>
+      <div className='grid h-52 w-52 place-content-center rounded-full bg-emerald-500 shadow-2xl'>
+        <div className='grid h-32 w-32 place-content-center rounded-full bg-teal-200'>
+          <div className='h-16 w-16 rounded-full bg-red-500'></div>
         </div>
       </div>
     </div>
